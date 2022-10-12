91c3945490d1cc5927da1d23602d0bf0e64955e4
Nahuda <114850987+Nahuda@users.noreply.github.com>
2022-10-11 23:45:48 -0800

    Update Farkle.py
    Cleaned up / commented main.
    Created meld choice input validation.
    Next, create algorithm for adding chosen melds to running total score, subtracting selected dice, and choosing to roll again or end turn.

--- Farkle.py
+++ Farkle.py
@@ -4,8 +4,8 @@ import time
 import numpy as np
 
 
+# Reset the game header.
 def refresh():
-    # Reset the game header.
     print("\033c")
     print('''                         ______         _    _      
                         |  ____|       | |  | |     
@@ -16,14 +16,13 @@ def refresh():
                      ──────────────────────────────────\n''')
 
 
+# Generate a dice roll result for a given number of dice.
 def roll_dice(num_dice):
-    # Generate a dice roll result for a given number of dice.
-    roll = list(np.sort(np.random.randint(1, 7, num_dice)))
-    return roll
+    return (roll := list(np.sort(np.random.randint(1, 7, num_dice))))
 
 
+# Illustrate a given dice roll and list the results.
 def print_dice(roll):
-    # Illustrate a given dice roll and list the results.
     die_top    = "┌─────────┐  "
     die_blank  = "│         │  "
     die_left   = "│  ●      │  "
@@ -64,13 +63,11 @@ def print_dice(roll):
     print()
     print(die_bottom * len(roll))
 
-    str_roll = [str(x) for x in roll]
-    print(f"Roll Result: {', '.join(str_roll)}")
+    print(f"Roll Result: {', '.join([str(x) for x in roll])}")
 
 
-def list_melds(roll):
-    # Analyze a given roll result for possible melds.
-    
+# Analyze a given roll result for possible melds.
+def list_melds(roll):    
     roll_dist = []
     valid_melds = []
 
@@ -113,41 +110,64 @@ def list_melds(roll):
 
 
 def main():
-    #@TODO - Clean up and comment main and create player turn function.
+    #@TODO - Create player turn function.
     refresh()
-    # Player starts their turn by throwing all six dice.
-    # Generate player's first dice throw
+    time.sleep(1)
+
+    # Begin Player Turn
+    print("It is your turn!")
+    input("Press Enter to roll.")
+    refresh()
+
+    print("With a vigorous shake, you roll the dice across the table.")
+    time.sleep(2)
+    refresh()
+    
+    # Generate new roll with six dice, and analyze it for valid melds.
     pl_roll = roll_dice(6)
-    time.sleep(1)
-    print("With a vigorous shake, you roll the dice across the table.\n")
-    time.sleep(1)
-    refresh()
-    print_dice(pl_roll)
     pl_melds = list_melds(pl_roll)
+
     if len(pl_melds) == 0:
-        print("FARKLE! All points gained this round are lost, and your turn is over.\n")
-    elif len(pl_melds) == 1:
-        print(f"\nYour only possible meld is: {'; '.join(pl_melds)}\n")
-    else:
-        print(f"\nYour highest possible melds are: {'; '.join(pl_melds)}\n")
-    while True:
-        meld_choice = input("Enter the dice values you would like to meld (1, 2, 3, etc.): ")
-        meld_choice = meld_choice.split(", ")
-        meld_choice = [int(x) for x in meld_choice]
-        choice_melds = list_melds(meld_choice)
-        if len(choice_melds) > 0 and choice_melds[0] in pl_melds:
-            print(choice_melds[0])
-            break
-        refresh()
+        # If no valid melds, FARKLE!
         print_dice(pl_roll)
-        if len(pl_melds) == 0:
-            print("FARKLE! All points gained this round are lost, and your turn is over.")
-        elif len(pl_melds) == 1:
-            print(f"\nYour only possible meld is: {'; '.join(pl_melds)}")
-        else:
-            print(f"\nYour highest possible melds are: {'; '.join(pl_melds)}")
-        #@TODO - Actual input validation.
-        print("\nERROR: Entered values do not meld.")
+        print("\nFARKLE! All points gained this round are lost, and your turn is over.\n")
+    else:
+        # Meld choice input validation loop.
+        i = 0
+        while i < 9001:
+            print_dice(pl_roll)
+            
+            if len(pl_melds) == 1:
+                print(f"\nYour only possible meld is: {'; '.join(pl_melds)}\n")
+            else:
+                print(f"\nYour highest possible melds are: {'; '.join(pl_melds)}\n")
+            
+            if i > 0:
+                print("ERROR: Entered values do not meld. Try again.", end="")
+
+            meld_choice = input("\nEnter the dice values you would like to meld (x, y, z, etc.): ")
+            meld_choice = meld_choice.split(", ")
+
+            try:
+                int_choice = [int(x) for x in meld_choice]
+                # Analyze the entered values for valid melds.
+                choice_melds = list_melds(int_choice)
+
+                # If chosen meld does not match any valid meld from the roll, redo input.
+                if len(choice_melds) > 0 and choice_melds[0] in pl_melds:
+                    break
+                # If entered meld(s) are 1s or 5s, just make sure they don't input more than they rolled.
+                elif (1 in pl_roll and 1 in int_choice) or (5 in pl_roll and 5 in int_choice):
+                    if (int_choice.count(1) <= pl_roll.count(1)) and (int_choice.count(5) <= pl_roll.count(5)):
+                        break
+                raise ValueError
+
+            # If no valid melds found from user input, try again and display error message.
+            except ValueError:
+                refresh()
+                i = 1
+    
+    print(choice_melds)
         
 
 main()
